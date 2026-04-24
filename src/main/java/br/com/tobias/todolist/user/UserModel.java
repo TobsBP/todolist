dcdf962a2d844355e3bcae90233535b440163c36
Tobias <tobias.bp2004@gmail.com>
2026-04-24 14:59:19 -0300

    feat: user table db

--- src/main/java/br/com/tobias/todolist/user/UserModel.java
+++ src/main/java/br/com/tobias/todolist/user/UserModel.java
@@ -1,10 +1,26 @@
 package br.com.tobias.todolist.user;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
+@Entity(name = "users")
 public class UserModel {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
 
     String username;
     String name;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 }
