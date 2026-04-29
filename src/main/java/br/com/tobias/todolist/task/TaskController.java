d3a0717ac88a42c405d683b29736d503853fc8a2
Tobias <tobias.bp2004@gmail.com>
2026-04-28 21:27:54 -0300

    feat: validate date

--- src/main/java/br/com/tobias/todolist/task/TaskController.java
+++ src/main/java/br/com/tobias/todolist/task/TaskController.java
@@ -1,5 +1,6 @@
 package br.com.tobias.todolist.task;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,13 +18,37 @@ public class TaskController {
     
     @Autowired
     private ITaskRepository taskRepository;
-
+    
+    public void validateDates(TaskModel taskModel) {
+        var now = LocalDateTime.now();
+    
+        if (taskModel.getStartAt() == null || taskModel.getEndAt() == null) {
+            throw new IllegalArgumentException("Dates cannot be null");
+        }
+    
+        if (now.isAfter(taskModel.getStartAt()) || now.isAfter(taskModel.getEndAt())) {
+            throw new IllegalArgumentException("Dates must be in the future");
+        }
+    
+        if (taskModel.getStartAt().isAfter(taskModel.getEndAt())) {
+            throw new IllegalArgumentException("Start date must be before end date");
+        }
+    }
+    
     @PostMapping("/task")
     public ResponseEntity<?> create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
         
         var userID = request.getAttribute("userID");
         
         taskModel.setUserID((UUID) userID);
+        
+        try {
+            validateDates(taskModel);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity
+                    .status(400)
+                    .body(e.getMessage());
+        }
         
         this.taskRepository.save(taskModel);  
         
